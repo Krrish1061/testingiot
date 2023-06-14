9c04ba7183277ee5cb372143329eb7b6a3b03268
Krishna Sapkota <krrish1061@gmail.com>
2023-06-14 17:51:33 +0545

    corrected urls

--- iot/urls.py
+++ iot/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html")),
+    path("", TemplateView.as_view(template_name="base.html")),
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
     path("user/", include("users.urls")),
