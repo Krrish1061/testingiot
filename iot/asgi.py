4293b4f8b3fe6a5abb4a42f5c5dd6c196f867920
Krishna Sapkota <krrish1061@gmail.com>
2024-02-08 18:21:09 +0545

    daphne appnotready error

--- iot/asgi.py
+++ iot/asgi.py
@@ -8,16 +8,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
 from django.core.asgi import get_asgi_application
-import websocket.routing
-from websocket.authmiddleware import JwtAuthMiddlewareStack
-from decouple import config
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iot.settings")
 django_asgi_app = get_asgi_application()
+
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+from decouple import config
+import websocket.routing
+from websocket.authmiddleware import JwtAuthMiddlewareStack
+
 
 application = ProtocolTypeRouter(
     {
