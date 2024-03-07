dac02f6394c78cda54e783584e2853cb9336a876
Krishna Sapkota <krrish1061@gmail.com>
2024-03-07 16:03:10 +0545

    debugging company name not editing

--- company/serializers.py
+++ company/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 from .models import Company, CompanyProfile
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class CompanyProfileSerializer(serializers.ModelSerializer):
@@ -43,10 +47,14 @@ class CompanySerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         name = validated_data.get("name")
+        logger.warning(
+            f"company name update method--new--{name} ---old---{instance.name}"
+        )
         user_limit = validated_data.get("user_limit")
         if name != instance.name:
             instance.name = name
         if user_limit != instance.user_limit:
             instance.user_limit = user_limit
         instance.save()
+        logger.warning(f"company name update method--{instance.name}")
         return instance
