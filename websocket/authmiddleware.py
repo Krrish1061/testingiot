3313c2966d1d18310db4d88b0e22615bc41cc99e
Krishna Sapkota <krrish1061@gmail.com>
2024-09-18 12:29:14 +0545

    removed logging in websocket auth

--- websocket/authmiddleware.py
+++ websocket/authmiddleware.py
@@ -12,9 +12,6 @@ from django.utils import timezone
 
 from users.cache import UserCache
 from websocket.cache import WebSocketCache
-import logging
-
-logger = logging.getLogger(__name__)
 
 
 @database_sync_to_async
@@ -37,27 +34,22 @@ class JwtAuthMiddleware(BaseMiddleware):
         # Get the token from query params
         token_query_string = parse_qs(scope["query_string"].decode("utf8"))
         token = token_query_string.get("token", [None])[0]
-        logger.warning(f"inside JwtAuthMiddleware websocket method initiated 1")
+
         if token:
             # get the token object from the database
             token_obj, user = await get_token(token)
-            logger.warning(f"inside JwtAuthMiddleware websocket method initiated 2")
+
             if token_obj:
                 # set the user
                 scope["user"] = user
                 company_slug = (
                     user.company.slug if user.is_associated_with_company else None
                 )
-                logger.warning(
-                    f"inside JwtAuthMiddleware websocket method initiated by user {user.username}-{user.type}-{company_slug}"
-                )
+
                 return await super().__call__(scope, receive, send)
 
         # to disconnect the websocket connection a bit slow
         # probably need to throw channels.exceptions.StopConsumer exception check docs Closing Consumers
-        logger.warning(
-            f"outside JwtAuthMiddleware websocket method initiated by user {user.username}-{user.type}-{company_slug}"
-        )
         return None
 
     # another approach is to accept connection as annoomous user
--- websocket/views.py
+++ websocket/views.py
@@ -5,9 +5,6 @@ from rest_framework.response import Response
 from websocket.cache import WebSocketCache
 from .models import WebSocketToken
 from .utilis import generate_token_key
-import logging
-
-logger = logging.getLogger(__name__)
 
 
 # Create your views here.
@@ -24,7 +21,4 @@ def get_websocket_token(request):
             user=request.user, token=token_key
         )
         WebSocketCache.set_websocket_token(user.username, websocket_token)
-    logger.warning(
-        f"inside get_websocket_token view websocket {user.username} {user.type}"
-    )
     return Response({"token": websocket_token.token}, status=status.HTTP_200_OK)
